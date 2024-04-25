24e1975a16a7ecf36c0297485e293f9fcde61c97
sunny1130 <sunmeet.singh1130@gmail.com>
2024-04-25 16:21:27 -0700

    Implement Kafka Producer/Receiver

---
+++ apache-camel-microservice-a/src/main/java/com/example/apachecamelmicroservicea/routes/MyKafkaJSONProducerRoute.java
@@ -0,0 +1,17 @@
+package com.example.apachecamelmicroservicea.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyKafkaJSONProducerRoute extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		from("timer:kafka-json-timer")
+		.transform().constant("{\"id\":\"100\",\"test\":\"JSON message\"}")
+		.to("kafka:myJSONkafkatopic");
+		
+	}
+
+}
---
+++ apache-camel-microservice-b/src/main/java/com/example/apachecamelmicroserviceb/routes/MyKafkaJSONReceiverRoute.java
@@ -0,0 +1,24 @@
+package com.example.apachecamelmicroserviceb.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.apachecamelmicroserviceb.beans.MyTest;
+
+@Component
+public class MyKafkaJSONReceiverRoute extends RouteBuilder{
+
+	@Autowired Header header;
+	@Override
+	public void configure() throws Exception {
+		from("kafka:myJSONkafkatopic")
+		.log("${body}")
+		.to("log:Camel")
+		.unmarshal().json(JsonLibrary.Jackson,MyTest.class)
+		.log("${body}")
+		.to("log:Camel")
+		.bean(header);
+	}
+}
