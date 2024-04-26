722e5ad4a78ff6517093459b1658ab49e566b231
sunny1130 <sunmeet.singh1130@gmail.com>
2024-04-25 17:43:36 -0700

    Creating Complex Deciders

--- apache-camel-microservice-a/src/main/java/com/example/apachecamelmicroservicea/routes/MyRESTRoute.java
+++ apache-camel-microservice-a/src/main/java/com/example/apachecamelmicroservicea/routes/MyRESTRoute.java
@@ -1,11 +1,17 @@
 package com.example.apachecamelmicroservicea.routes;
 
+import java.util.Map;
+
+import org.apache.camel.Body;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRESTRoute extends RouteBuilder{
 
+	@Autowired DeciderBean deciderBean;
 	@Override
 	public void configure() throws Exception {
 		
@@ -13,14 +19,30 @@ public class MyRESTRoute extends RouteBuilder{
 		
 		from("timer:rest-timer?period=10000")
 		.to("direct:consolelog")
-		.to("rest:get:/hello")
-		.to("direct:consolelog")
-		.to("rest:get:/mytest")
-		.to("direct:consolelog");
+		.choice()
+			.when(method(deciderBean,"checknull"))
+				.to("rest:get:/hello")
+				.to("direct:consolelog")
+				.to("rest:get:/mytest")
+				.to("direct:consolelog")
+			.otherwise()
+				.log("Body is null")
+		.end();
 		
 		from("direct:consolelog")
 		.to("log:log");
 		
 	}
 
+}
+
+@Component
+class DeciderBean{
+	
+	public boolean checknull(@Body String body, @Headers Map<String,String> header) {
+		if(null != body)
+			return true;
+		else
+			return false;
+	}
 }
